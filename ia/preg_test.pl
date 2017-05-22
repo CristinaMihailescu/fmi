312e61ce5027dcbfb2cdf3e1369042d0068971d9
Cristina Mihailescu <mihailescucristina@outlook.com>
2017-05-22 14:22:23 +0300

    Update preg_test.pl

--- ia/preg_test.pl
+++ ia/preg_test.pl
@@ -5,11 +5,10 @@ verif(E, [E|L], N) :- verif(E, L, N), !.
 verif(E, [F|L], N) :- E mod F =:= 0, F =\= 1, verif(E, L, N1), N is N1 + 1, !.
 verif(E, [_|L], N) :- verif(E, L, N).
 
-rezolva(_, [], []).
-rezolva(I, [E|L], [E|R]) :- verif(E, I, N), N >= 2, rezolva(I, L, R), !.
-rezolva(I, [_|L], R) :- rezolva(I, L, R).
+membru(E, [E | _]).
+membru(E, [_ | L]) :- membru(E, L).
 
-prima_problema(L, R) :- rezolva(L, L, R).
+prima_problema(L, R) :- findall(E, (membru(E, L), verif(E, L, N), N >= 2), R).
 
 % 2) Scrieti un predicat a_doua_problema(A) care primeste A un arbore binar oarecare cu elemente numere naturale si afiseaza pe ecran, fiecare pe cate o linie, perechile de noduri "frati" care au aceeasi paritate.
 % A = arb(arb(arb(arb(nil, 8, nil), 3, arb(nil, 10, nil)), 2, arb(nil, 4, arb(nil, 9, nil))), 7, arb(arb(nil, 1, nil), 6, arb(arb(nil, 13, nil), 5, arb(nil, 11, nil))))
