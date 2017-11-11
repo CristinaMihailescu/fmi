49bc5eb25e5ae87b19de2f4650cb584cb6f51a43
Cristina Mihailescu <mihailescucristina@outlook.com>
2017-11-11 07:21:30 +0200

    update lab 5

--- pd/lab5/Lab5.hs
+++ pd/lab5/Lab5.hs
@@ -10,15 +10,15 @@ import Test.QuickCheck
 -- 1. Map
 -- a. (4 simboluri)
 uppers :: String -> String
-uppers = undefined
+uppers xs = map toUpper xs
 
 -- b. (7 simboluri)
 doubles :: [Int] -> [Int]
-doubles = undefined
+doubles xs = map (\x -> 2*x) xs
 
 -- c. (10 simboluri)
-penceToPounds :: [Int] -> [Float]
-penceToPounds = undefined
+penceToPounds :: [Integer] -> [Float]
+penceToPounds xi = map (\x -> realToFrac x / 100) xi --HELLLLLLLLLLOOOOOOOOOOOOOOOOOOOO
 
 -- d. (11 simboluri)
 uppers' :: String -> String
@@ -33,19 +33,19 @@ prop_uppers = undefined
 -- 2. Filter
 -- a. (4 simboluri)
 alphas :: String -> String
-alphas = undefined
+alphas xs = filter isAlpha xs
 
 -- b. (8 simboluri)
 rmChar ::  Char -> String -> String
-rmChar = undefined
+rmChar c xs = filter (/= c) xs
 
 -- c. (8 simboluri)
 above :: Int -> [Int] -> [Int]
-above = undefined
+above m xi = filter (> m) xi
 
 -- d. (13 simboluri)
 unequals :: [(Int,Int)] -> [(Int,Int)]
-unequals = undefined
+unequals xi = filter (\(x, y) -> x /= y) xi
 
 -- e. (15 simboluri)
 rmCharComp :: Char -> String -> String
@@ -64,7 +64,7 @@ upperChars s = [toUpper c | c <- s, isAlpha c]
 
 -- (7 simboluri)
 upperChars' :: String -> String
-upperChars' = undefined
+upperChars' xs = map toUpper(filter isAlpha xs)
 
 prop_upperChars :: String -> Bool
 prop_upperChars s = upperChars s == upperChars' s
@@ -75,7 +75,7 @@ largeDoubles xs = [2 * x | x <- xs, x > 3]
 
 -- (13 simboluri)
 largeDoubles' :: [Int] -> [Int]
-largeDoubles' = undefined
+largeDoubles' xs = map (* 2) (filter (> 3) xs)
 
 prop_largeDoubles :: [Int] -> Bool
 prop_largeDoubles xs = largeDoubles xs == largeDoubles' xs 
@@ -86,7 +86,7 @@ reverseEven strs = [reverse s | s <- strs, even (length s)]
 
 -- (11 simboluri)
 reverseEven' :: [String] -> [String]
-reverseEven' = undefined
+reverseEven' xs = map reverse (filter (even.length) xs)
 
 prop_reverseEven :: [String] -> Bool
 prop_reverseEven strs = reverseEven strs == reverseEven' strs
@@ -101,7 +101,7 @@ productRec (x:xs) = x * productRec xs
 
 -- (7 simboluri)
 productFold :: [Int] -> Int
-productFold = undefined
+productFold xi = foldr (*) 1 xi
 
 prop_product :: [Int] -> Bool
 prop_product xs = productRec xs == productFold xs
@@ -112,7 +112,7 @@ andRec = undefined
 
 -- (7 simboluri)
 andFold :: [Bool] -> Bool
-andFold = undefined
+andFold xb = foldr (&&) True xb
 
 prop_and :: [Bool] -> Bool
 prop_and xs = andRec xs == andFold xs 
@@ -123,7 +123,7 @@ concatRec = undefined
 
 -- (8 simboluri)
 concatFold :: [[a]] -> [a]
-concatFold = undefined
+concatFold xs = foldr (++) [] xs
 
 prop_concat :: [String] -> Bool
 prop_concat strs = concatRec strs == concatFold strs
@@ -147,17 +147,18 @@ type Matrix = [[Int]]
 -- 5
 -- a. (10 simboluri)
 uniform :: [Int] -> Bool
-uniform = undefined
+uniform xi = all (== head xi) xi
 
 -- b. (	 simboluri)
 valid :: Matrix -> Bool
-valid = undefined
+valid xi = uniform (map length xi) && (length (head xi)) > 0 && length xi > 0
 
 -- 6.
 
 -- 7.  (22 simboluri + 19 simboluri)  cu tot cu tratarea erorilor
 plusM :: Matrix -> Matrix -> Matrix
-plusM = undefined
+plusM m1 m2 = if valid m1 && valid m2 then zipWith(zipWith (+)) m1 m2
+              else error "Invalid input"
 
 -- 8. (23 simboluri + 15 simboluri)  cu tot cu tratarea erorilor  
 timesM :: Matrix -> Matrix -> Matrix
